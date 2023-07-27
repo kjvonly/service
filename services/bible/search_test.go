7a973346e170f5f6a2e9be182aff129c43b8c5dc
kjvonly <136278437+kjvonly@users.noreply.github.com>
2023-07-26 21:25:45 -0400

    adds code to search file

--- services/bible/search_test.go
+++ services/bible/search_test.go
@@ -1,16 +1,2 @@
 package bible
 
-type SearchService interface {
-}
-
-type Storer interface {
-}
-
-type SearchRequest struct {
-	Search Search `json:"search"`
-}
-
-type SearchResponse struct {
-	SearchResults SearchResults `json:"search_results"`
-	Error         string        `json:"error,omitempty"`
-}
